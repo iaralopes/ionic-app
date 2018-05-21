7328c6562a28166a6eb5a1f8ff88af88d4fc38ef
iaralopes <iarasiria@gmail.com>
2018-05-21 00:04:11 -0300

    Alteração da apiUrl

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RestProvider {
 
-  apiUrl = "http://localhost:8080";
+  apiUrl = "https://shielded-garden-64520.herokuapp.com";
 
   constructor(public http: HttpClient) {
     console.log('Hello RestProvider Provider');
